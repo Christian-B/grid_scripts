bd92f8eb36824ca5cf329fe7c95bf2676e13f73e
= <christian.brenninkmeijer@manchester.ac.uk>
2015-06-17 10:22:44 +0100

    Count by chrom

--- python/countBamAlignment.py
+++ python/countBamAlignment.py
@@ -1,17 +1,27 @@
 import argparse
 import brenninc_utils
+import collections
 import HTSeq
 import os
-
 
 def count_single_file(bam_file, handle):
     print "counting alignments in " + bam_file
     alignment_iterator = HTSeq.BAM_Reader(bam_file)
-    count = 0
+    all_count = 0
+    unaligned_count = 0
+    chrom_counter = collections.Counter()
     for alignment in alignment_iterator:
-        count += 1
-    handle.write(str(count) + "  " + bam_file + "\n")
-
+        all_count += 1
+        if alignment.iv is None:
+            unaligned_count += 1
+        else:            
+            chrom_counter[alignment.iv.chrom] += 1
+    handle.write("Summary for " + bam_file + "\n")
+    handle.write("Chrom".rjust(15) + "count".rjust(12) + "\n")
+    for chrom in sorted(chrom_counter):  
+        handle.write(chrom.rjust(15) + "{0:12d}".format(chrom_counter[chrom]) + "\n")
+    handle.write("Unaligned".rjust(15) + "{0:12d}".format(unaligned_count) + "\n")
+    handle.write("Total".rjust(15) + "{0:12d}".format(all_count) + "\n")
 
 def count_alignments(path=os.getcwd(), output_file=None):
     files = brenninc_utils.find_files(path, ['bam', 'bam.gz'])
@@ -35,6 +45,10 @@ if __name__ == '__main__':
                         "will be read. "
                         "Files ending in '.gz' "
                         "will automatically be unzipped. ")
+
+    #group = parser.add_mutually_exclusive_group()
+    #group.add_argument('-a', action='store_true')
+    #group.add_argument('-b', action='store_true')
 
     args = parser.parse_args()
     print args
